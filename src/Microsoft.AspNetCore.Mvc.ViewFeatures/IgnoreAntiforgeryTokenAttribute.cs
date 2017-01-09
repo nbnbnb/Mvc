a2321c78e3851c560deb6446abceb35a012b0b50
Kiran Challa <kichalla@microsoft.com>
2017-01-09 14:14:21 -0800

    Added comment

--- src/Microsoft.AspNetCore.Mvc.ViewFeatures/IgnoreAntiforgeryTokenAttribute.cs
+++ src/Microsoft.AspNetCore.Mvc.ViewFeatures/IgnoreAntiforgeryTokenAttribute.cs
@@ -13,6 +13,10 @@ namespace Microsoft.AspNetCore.Mvc
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class IgnoreAntiforgeryTokenAttribute : Attribute, IAntiforgeryPolicy, IOrderedFilter
     {
+        // NOTE: The default order of this attribute must match with that of ValidateAntiForgeryToken and
+        // AutoValidateAntiForgeryToken attributes. This is so that in the scenarios where these attributes are used
+        // at a broader scope, the most local ignore attribute (if any) should be considered.
+
         /// <inheritdoc />
         public int Order { get; set; } = 1000;
     }
